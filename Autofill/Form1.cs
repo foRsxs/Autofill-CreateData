7e1b02150b02dda9ad600f80e90d112ebc3cbbbb
Dmitriy <foRs.xs@gmail.com>
2017-02-16 18:28:32 +0600

    Start

--- Autofill/Form1.cs
+++ Autofill/Form1.cs
@@ -35,6 +35,7 @@ namespace Autofill
         //System.Windows.Forms.Timer timerClicker1;
         System.Windows.Forms.Timer timerClicker2;
         //  bool time_to_do = true;
+        // test
         Options op;
         bool check, err;
         bool work_o = false;
